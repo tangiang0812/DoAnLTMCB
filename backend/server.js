e57b06b0871e6d9ac39708d0581371fe454d1954
Gnaig <tangiang0812@gmail.com>
2022-04-25 00:45:48 +0700

    first commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const chats = require('./data/data');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Serving API');
+})
+
+app.get('/api/chat', (req, res) => {
+    res.send(chats);    
+})
+
+app.get('/api/chat/:id', (req, res) => {
+    const singleChat = chats.find(c => c._id === req.params.id);
+    res.send(singleChat);
+})
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`LISTENING ON PORT ${PORT}`);
+})
