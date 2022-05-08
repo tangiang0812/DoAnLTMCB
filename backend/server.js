cf2d22b2dde935ca34635066ae524542165f744d
Gnaig <tangiang0812@gmail.com>
2022-05-08 23:54:42 +0700

    connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,28 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
-const chats = require('./data/data');
+const chats = require("./data/data");
 
 const app = express();
+dotenv.config();
+connectDB();
 
-app.get('/', (req, res) => {
-    res.send('Serving API');
-})
+app.get("/", (req, res) => {
+  res.send("Serving API ");
+});
 
-app.get('/api/chat', (req, res) => {
-    res.send(chats);    
-})
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
 
-app.get('/api/chat/:id', (req, res) => {
-    const singleChat = chats.find(c => c._id === req.params.id);
-    res.send(singleChat);
-})
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log(`LISTENING ON PORT ${PORT}`);
-})
+  console.log(`LISTENING ON PORT ${PORT}`);
+});
